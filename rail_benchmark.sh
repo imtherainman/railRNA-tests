d1c003906188f346c8bcdd852eba7d756b334466
rcao5 <rcao5@jhu.edu>
2017-10-06 07:46:52 -0400

    added top batch mode

--- rail_benchmark.sh
+++ rail_benchmark.sh
@@ -2,25 +2,36 @@
 
 while getopts p:d:o:t:m: option
 do
- case "${option}"
- in
- p) PROCS=${OPTARG};;
- d) INTERM=${OPTARG};;
- o) BT1=${OPTARG};;
- t) BT2=${OPTARG};;
- m) MANIFEST=${OPTARG};;
- esac
+	case "${option}"
+	in
+	p) PROCS=${OPTARG};;
+	d) INTERM=${OPTARG};;
+	o) BT1=${OPTARG};;
+	t) BT2=${OPTARG};;
+	m) MANIFEST=${OPTARG};;
+	esac
 done
 
 BASEDIR=$(dirname "$0")
+mkdir $BASEDIR/top_logs
+mkdir $BASEDIR/run_logs
 
 # loop over all core counts
 for CORES in $(echo $PROCS | tr "," "\n")
 do
-  echo Running Rail with $CORES processes
-  rail-rna go local -x $BT1 $BT2 -m $MANIFEST -p $CORES --scratch $INTERM
+	echo Running Rail with $CORES cores
 
-  cp $BASEDIR/rail-rna_logs/*.log $BASEDIR/$CORES.log
-  rm -rf $BASEDIR/rail-rna_logs/
-  rm -rf $BASEDIR/rail-rna_out/
+	# start recording with top in background
+	top -d 2 -b|grep --line-buffered "load average" -A 12 > $BASEDIR/top_logs/top_core_$CORES.log &
+	TOPPID=$!
+
+	# start rail
+	rail-rna go local -x $BT1 $BT2 -m $MANIFEST -p $CORES --scratch $INTERM
+
+	# kill background top
+	kill $TOPPID
+
+	cp $BASEDIR/rail-rna_logs/*.log $BASEDIR/run_logs/$CORES.log
+	rm -rf $BASEDIR/rail-rna_logs/
+	rm -rf $BASEDIR/rail-rna_out/
 done
