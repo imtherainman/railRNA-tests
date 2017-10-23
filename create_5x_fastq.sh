540d1f56fd38940685d864b8039779c4d55c05a1
rcao5 <rcao5@jhu.edu>
2017-10-23 12:41:27 -0400

    create 5x fastq for right

--- create_5x_fastq.sh
+++ create_5x_fastq.sh
@@ -4,4 +4,6 @@ for i in {1..5}
 do
 	cat dm3_example_1_left.fastq  >> dm3_example_1_left_x5.fastq
 	cat dm3_example_2_left.fastq >> dm3_example_2_left_x5.fastq
+	cat dm3_example_1_right.fastq >> dm3_example_1_right_x5.fastq
+	cat dm3_example_2_right.fastq >> dm3_example_2_right_x5.fastq
 done
