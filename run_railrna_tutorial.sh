4b17c14f706dd8d64331db0e9422347df87e632e
rcao5 <rcao5@jhu.edu>
2017-12-20 10:13:11 -0600

    switch back to /tmp

--- run_railrna_tutorial.sh
+++ run_railrna_tutorial.sh
@@ -7,8 +7,8 @@ BASEDIR=$(dirname "$0")
 
 for (( n_rails=$START; n_rails<=$END; n_rails+=1 ))
 do
-	echo "${n_rails} instances" >> /home1/05096/rcao/testing/runtimes_${n_rails}_scratch.txt
-	{ time sh $BASEDIR/rail_benchmark_parallel.sh -o /home1/05096/rcao/testing/Drosophila_melanogaster/UCSC/dm3/Sequence/BowtieIndex/genome -t /home1/05096/rcao/testing/Drosophila_melanogaster/UCSC/dm3/Sequence/Bowtie2Index/genome -m https://raw.githubusercontent.com/nellore/rail/master/ex/dm3_example.manifest -p $CORES -d /scratch -n $n_rails 2> rail.stderr ; } 2>> /home1/05096/rcao/testing/runtimes_${n_rails}_scratch.txt
+	echo "${n_rails} instances" >> /home1/05096/rcao/testing/runtimes_${n_rails}.txt
+	{ time sh $BASEDIR/rail_benchmark_parallel.sh -o /home1/05096/rcao/testing/Drosophila_melanogaster/UCSC/dm3/Sequence/BowtieIndex/genome -t /home1/05096/rcao/testing/Drosophila_melanogaster/UCSC/dm3/Sequence/Bowtie2Index/genome -m https://raw.githubusercontent.com/nellore/rail/master/ex/dm3_example.manifest -p $CORES -d /tmp -n $n_rails 2> rail.stderr ; } 2>> /home1/05096/rcao/testing/runtimes_${n_rails}.txt
 	rm -rf /home1/05096/rcao/testing/parallel_logs/logs_*_rail/*out*/
 done
 
